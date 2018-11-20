c2408dfc927aa99f4656cea9abc47114d3a498ac
issei.takeuchi <takeuchi823823@gmail.com>
2018-11-20 20:51:02 +0900

    Add DestroyServlet.java

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -1,19 +1,25 @@
 package controllers;
 
 import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.tasks;
+import utils.DBUtil;
+
 /**
  * Servlet implementation class EditServlet
  */
-@WebServlet("/exit")
+@WebServlet("/edit")
 public class EditServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,8 +32,20 @@ public class EditServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	    EntityManager em = DBUtil.createEntityManager();
+
+        tasks m = em.find(tasks.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        request.setAttribute("tasks", m);
+        request.setAttribute("_token", request.getSession().getId());
+        request.getSession().setAttribute("tasks_id", m.getId());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasksviews/edit.jsp");
+        rd.forward(request, response);
+    }
 
 }
+
+
